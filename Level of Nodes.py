9f8aaf51e9698c5c61f79e0cf2428b0ade552999
Sufiyan Khan <ksufiyan38@gmail.com>
2023-10-19 23:15:21 +0530

    Create Level of Nodes.py

---
+++ Level of Nodes.py
@@ -0,0 +1,63 @@
+Level of Nodes
+MediumAccuracy: 50.95%Submissions: 45K+Points: 4
+Bugs Have Invaded! Register for Debugging Contest & Help us Squash 'em!
+
+banner
+Given an integer X and an undirected acyclic graph with V nodes, labeled from 0 to V-1, and E edges, find the level of node labeled as X.
+
+Level is the minimum number of edges you must travel from the node 0 to some target.
+
+If there doesn't exists such a node that is labeled as X, return -1.
+
+Example 1:
+
+Input:
+
+X = 4
+Output:
+2
+Explanation:
+
+We can clearly see that Node 4 lies at Level 2.
+Example 2:
+
+Input:
+
+X = 1
+Output:
+1
+Explanation:
+Node 1 lies at level 1, immediate after Node 0.
+Your task:
+You dont need to read input or print anything. Your task is to complete the function nodeLevel() which takes two integers V and X denoting the number of vertices and the node, and another adjacency list adj as input parameters and returns the level of Node X. If node X isn't present it returns -1.
+
+Expected Time Complexity: O(V)
+Expected Auxiliary Space: O(V)
+
+Constraints:
+2 ≤ V ≤ 104
+1 ≤ E ≤ 104
+0 ≤ adji, j < V
+1 ≤ X < V
+Graph doesn't contain multiple edges and self loops.
+
+
+
+Solution:
+class Solution:
+    def nodeLevel(self, V, adj, X):
+        seen=set()
+        q=[0]
+        lvl=0
+        while q:
+            nq=[]
+            for i in q:
+                seen.add(i)
+                if i==X:
+                    return lvl
+                for j in adj[i]:
+                    if j not in seen:
+                        nq.append(j)
+            q=nq
+            lvl+=1
+        return -1
